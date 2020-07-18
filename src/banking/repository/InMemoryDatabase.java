e6e5b171efd4dc4f2956ccee0da70fc62802226c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-18 19:23:35 +0300

    Added implementation of method getAccount in class InMemoryDatabase

--- src/banking/repository/InMemoryDatabase.java
+++ src/banking/repository/InMemoryDatabase.java
@@ -3,12 +3,21 @@ package banking.repository;
 import banking.Account;
 
 import java.util.List;
+import java.util.Optional;
 
 public class InMemoryDatabase implements AccountsRepository {
     private List<Account> accounts;
 
     @Override
     public void addAccount(Account account) {
+        accounts.add(account);
+    }
 
+    @Override
+    public Optional<Account> getAccount(String creditCardNumber, String pinNumber) {
+        return accounts.stream()
+                .filter(account -> account.getCardNumber().equals(creditCardNumber)
+                                && account.getPinNumber().equals(pinNumber))
+                .findFirst();
     }
 }
