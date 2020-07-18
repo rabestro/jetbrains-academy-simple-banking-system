8800b2e18f592ea51ad13e2707cf79d12f641c16
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-18 18:36:25 +0300

    Added constructor for class Account

--- src/banking/Account.java
+++ src/banking/Account.java
@@ -4,12 +4,17 @@ public class Account {
     private static final String IIN = "400000";
     private static long lastAccountNumber = 1;
 
-    private long customerAccountNumber;
-    private int checksum = 0;
-
+    private final long customerAccountNumber;
+    private final int checksum;
     private String pin;
+
+    public Account() {
+        customerAccountNumber = lastAccountNumber++;
+        checksum = 0;
+    }
 
     public String getCardNumber() {
         return String.format("%s%09d%d", IIN, customerAccountNumber, checksum);
     }
+
 }
