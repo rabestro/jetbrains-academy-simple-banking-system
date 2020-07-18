25310ac6468aba5976c0c25a623034197a2c1ec9
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-18 18:45:48 +0300

    Added class InMemoryDatabase

---
+++ src/banking/InMemoryDatabase.java
@@ -0,0 +1,12 @@
+package banking;
+
+import java.util.List;
+
+public class InMemoryDatabase implements AccountsRepository {
+    private List<Account> accounts;
+
+    @Override
+    public void addAccount(Account account) {
+
+    }
+}
