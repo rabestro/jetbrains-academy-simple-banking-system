6530d9193bea2293f65d0a3af9048dc9bd1a0603
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-18 18:08:27 +0300

    Added logger configuration

--- src/banking/Main.java
+++ src/banking/Main.java
@@ -1,7 +1,23 @@
 package banking;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    static {
+        try {
+            LogManager.getLogManager().readConfiguration(
+                    new FileInputStream("logging.properties"));
+        } catch (IOException e) {
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+    }
     public static void main(String[] args) {
+        log.info("Simple Banking System started.");
         System.out.println("Hello World!");
     }
 }
