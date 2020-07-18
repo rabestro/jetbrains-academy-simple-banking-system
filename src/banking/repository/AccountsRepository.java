9680972d28dff5dbff9d40b416a78f2231ec6494
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-18 19:18:35 +0300

    Added method getAccount() in interface AccountsRepository

--- src/banking/repository/AccountsRepository.java
+++ src/banking/repository/AccountsRepository.java
@@ -2,6 +2,8 @@ package banking.repository;
 
 import banking.Account;
 
+import java.util.Optional;
+
 public interface AccountsRepository {
     void addAccount(Account account);
 
@@ -10,4 +12,6 @@ public interface AccountsRepository {
         addAccount(account);
         return account;
     }
+
+    Optional<Account> getAccount(final String creditCardNumber, final String pinNumber);
 }
