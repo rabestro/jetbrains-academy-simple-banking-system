8dd6ca41f64e4e326532a88ed9dbe452346f06c1
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-18 18:30:21 +0300

    Added static field lastAccountNumber in class Account

--- src/banking/Account.java
+++ src/banking/Account.java
@@ -2,7 +2,9 @@ package banking;
 
 public class Account {
     private static final String IIN = "400000";
+    private static long lastAccountNumber = 1;
 
+    private long customerAccountNumber;
     private String cardNumber;
     private String pin;
 }
