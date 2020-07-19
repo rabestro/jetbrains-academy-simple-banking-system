9aae50264e1d6ea626753862e02daf348d4cb3d8
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-19 23:58:34 +0300

    Created class SQLiteDatabase

---
+++ src/banking/repository/SQLiteDatabase.java
@@ -0,0 +1,28 @@
+package banking.repository;
+
+import banking.Account;
+
+import java.util.Optional;
+
+public class SQLiteDatabase implements AccountsRepository {
+    private final String databaseName;
+
+    public SQLiteDatabase(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    @Override
+    public void addAccount(Account account) {
+
+    }
+
+    @Override
+    public Account createAccount() {
+        return null;
+    }
+
+    @Override
+    public Optional<Account> getAccount(String creditCardNumber, String pinNumber) {
+        return Optional.empty();
+    }
+}
