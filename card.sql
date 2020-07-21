e735643b625d99b3e0c6c8fa31d704a5671e427f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-21 21:49:42 +0300

    Updated sql for table creating. Fixed issue #8

--- card.sql
+++ card.sql
@@ -1,6 +1,6 @@
 CREATE TABLE card (
-id INTEGER PRIMARY KEY,
-number TEXT,
-pin TEXT,
-balance INTEGER DEFAULT 0
+    id      INTEGER PRIMARY KEY,
+    number  TEXT NOT NULL UNIQUE CHECK ( length(number) = 16 ),
+    pin     TEXT NOT NULL CHECK ( length(pin) = 4 ),
+    balance INTEGER NOT NULL DEFAULT 0 CHECK ( balance >= 0 )
 );
